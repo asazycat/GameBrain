f4d7c99937a8836c46615d3ab807fcf47c3742af
Asad Jawaid <asazycat@gmail.com>
2025-08-29 11:52:10 +0100

    grid for list of games

--- src/Components/EachSearchResult.tsx
+++ src/Components/EachSearchResult.tsx
@@ -6,11 +6,14 @@ export default function EachSearchResult({ game }: {game: IGame}) {
     const { id, name, genre, rating, image } = game
     return (
         <Link to={`${id}`}>
-            <li key={id} className="border-2 border-solid  m-auto rounded-sm">
-                        <h1>{name}</h1>   
-                        <p>{genre }</p>
-                        <p>{ rating.mean ?? 'yet to be added'}</p>
-                        <img src={image} alt={name} className="min-sm:h-[20em] min-sm:w-[20em]"/>
+            <li key={id} className=" m-auto rounded-4xl p-5 max-sm:flex max-sm:flex-col max-sm:max-w-fit bg-[#284246] ">
+                <div className="m-auto p-3 min-w-fit "> 
+                    <h1 className=" text-[#FFFF] font-mono max-sm:text-2xl ">{name}</h1>   
+                    <p className=" max-sm:text-sm  text-center w-auto max-sm:max-w-fit border-2 bg-[#77858f] text-[#d2eb47] border-[#77858f] rounded-3xl">{genre }</p>
+                    <p>{rating.mean ?? 'yet to be added'}</p>
+                 </div>
+                    <img src={image} alt={name} className="max-sm:rounded-2xl max-sm:border-2 max-sm:border-solid max-sm:border-amber-400 min-sm:h-[20em] min-sm:w-[20em] max-sm:h-[7em] max-sm:w-[7em] m-auto"/>
+                  
             </li>
         </Link>
     )

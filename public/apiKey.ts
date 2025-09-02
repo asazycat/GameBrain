d6120ffd6cf9a3c9ca4c761327df20a807dfc505
Asad Jawaid <asazycat@gmail.com>
2025-09-03 00:32:07 +0100

    much better each game

--- public/apiKey.ts
+++ public/apiKey.ts
@@ -1,7 +1,7 @@
 //Change api key here, there is already a spare from my alt account but if it doesn't work, copy the key into the const variable from GameBrain.co
 
-const apiKey = 'd13baa727ae441a29f0db05328ea730d'
-// const apiKey = '17263356b8ed45c6a24c6c96e738d835'
+// const apiKey = 'd13baa727ae441a29f0db05328ea730d'
+const apiKey = '17263356b8ed45c6a24c6c96e738d835'
 export default apiKey
 
 

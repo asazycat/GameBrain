3ee5959d9181dcebbae764659ee28df41390f03c
Asad Jawaid <asazycat@gmail.com>
2025-09-04 09:14:30 +0100

    homepage tart

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -3,8 +3,14 @@
 
 export default function Homepage () {
     return (
-        <div className=" m-auto absolute top-1/2 -translate-y-1/2 w-full">
-        <h1 className="text-white text-center font-extrabold bg-[#284246]  m-auto w-85 p-3 border-[#62afb9] border-5 rounded-2xl  ">From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</h1>
+        <div className=" m-auto bg-[#284246]  border-2 solid p-2 w-9/10 mt-15">
+            <img src="../../public/image-not-available.jpg" className="m-auto"/>
+            <div className="flex bg-[#284246] w-full justify-around text-[#d2eb47] mt-5">
+                <button className="border-2 solid  m-2 rounded-2xl bg-[#77858f] text-[#d2eb47] border-[#77858f] ">Top PlayStation Games</button>
+                <button className="border-2 solid m-2 rounded-2xl bg-[#77858f] text-[#d2eb47] border-[#77858f] ">Top Xbox Games</button>
+                <button className="border-2 solid m-2 rounded-2xl bg-[#77858f] text-[#d2eb47] border-[#77858f] ">Top PC Games</button>
+            </div>
+        <h1 className="mt-5 text-white text-center font-extrabold bg-[#284246]  m-auto w-auto p-3 border-[#62afb9] border-5 rounded-2xl  ">From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</h1>
         
         </div>
             )

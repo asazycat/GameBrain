80c6ad11fb63dfc54bc3af8694e0c97d9b4a664d
Asad Jawaid <asazycat@gmail.com>
2025-09-07 21:33:10 +0100

    burger

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,12 +3,14 @@
 
 import Navigation from "./Navigation";
 import Profile from "./Profile";
+
 export default function Header() {
      
     return (
         <header className="flex justify-between p-2 bg-[#284246] max-sm:h-full max-sm:bg-[#1c2b2d] ">
             <Navigation/>
             <div className=" text-left size-fit">
+             
             <Profile/>
             </div>
             </header>
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -6,8 +6,8 @@ export default function Navigation() {
     
     return (
         <nav className='w-screen '>
-            <button className={`border-[#293f42] border-2 border-solid text-[#3887a3] max-sm: block  bg-[#1c2b2d] max-sm:border-[#1C2B2D] w-full p-2 min-sm:hidden `} onClick={() => setBurger(!burger)}><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p></button>
-            <ul className={`border-[#293f42] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex' : 'hidden min-sm:flex min-sm:flex-row'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#293f42] max-sm: text-left  text-[#3887a3] font-mono text-xl `}>
+            <button className={`border-[#293f42] border-2 border-solid text-[#3887a3] max-sm:block  bg-[#1c2b2d] max-sm:border-[#1C2B2D] w-full p-2 min-sm:hidden `} onClick={() => setBurger(!burger)}><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p></button>
+            <ul className={`border-[#284246] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex ' : 'hidden min-sm:flex min-sm:flex-row'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#284246] max-sm: text-left max-sm:mt-3  max-sm:-ml-2  text-[#3887a3] font-mono text-xl `}>
                         <li><Link to={`/`}>Home</Link></li>
                         <li><Link to={`/games`}>Games</Link></li>
                         <li><Link to={`/articles`}>Articles</Link></li>

40629549802483fc769e52a1efd6acde9ae95c89
Asad Jawaid <asazycat@gmail.com>
2025-09-07 19:13:55 +0100

    working on homepage

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -35,7 +35,7 @@ export default function Homepage() {
     return (
         <>
         <div className=" m-auto bg-[#284246]  border-2 solid p-2 w-9/10 mt-15">
-            
+            <img src="https://media.gq-magazine.co.uk/photos/645b5c3c8223a5c3801b8b26/16:9/w_1280,c_limit/100-best-games-hp-b.jpg?mbid=social_retweet" className="m-auto"/>
            <h1 className="mt-5 text-white text-center font-extrabold bg-[#284246]  m-auto w-auto p-3 border-[#62afb9] border-5 rounded-2xl  ">From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</h1>
             <div className="flex bg-[#284246] w-full justify-around text-[#d2eb47] mt-5">
                 <button className="border-2 solid  m-2 rounded-2xl bg-[#77858f] text-[#d2eb47] border-[#77858f] " onClick={e => setPlatform(e.currentTarget.value)} value='playstation'>Top PlayStation Games</button>

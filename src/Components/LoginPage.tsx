f5a0622ca4e20cf80c00fac457913db94272b24f
Asad Jawaid <asazycat@gmail.com>
2025-09-15 20:12:18 +0100

    bg-color

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -26,7 +26,7 @@ export default function LoginPage({ setTokenObj }: {  setTokenObj: Dispatch<SetS
         }
     
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col w-3/4 m-auto p-5 bg-[#284246]  border-2 solid rounded-2xl h-[10em] justify-between mt-50 min-sm:w-1/2 min-md:w-1/3 min-lg:w-1/4 min-2xl:w-1/3 min-2xl:h-[15em]">
+        <form onSubmit={handleSubmit} className="flex flex-col w-3/4 m-auto p-5 bg-[#1c2b2d]  border-2 solid rounded-2xl h-[10em] justify-between mt-50 min-sm:w-1/2 min-md:w-1/3 min-lg:w-1/4 min-2xl:w-1/3 min-2xl:h-[15em]">
             <input className='bg-white text-gray-400 min-2xl:h-[3em]' type="text" onChange={e => setUsername(e.target.value)} placeholder='Username' />
             <input className='bg-white text-gray-400 min-2xl:h-[3em]' type="password" onChange={e => setPassword(e.target.value)} placeholder='Password'/>
             <div className="flex flex-row justify-around">
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -7,11 +7,11 @@ export default function Navigation() {
     return (
         <nav className='w-1/2 max-sm:w-1/3'>
             <button className={`border-[#1c2b2d] border-2 border-solid text-[#3887a3] max-sm:block  bg-[#1c2b2d] max-sm:border-[#1C2B2D] w-full p-2 min-sm:hidden `} onClick={() => setBurger(!burger)}><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p></button>
-            <ul className={`hover:text-[#d2eb47] border-[#1c2b2d] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex ' : 'hidden min-sm:flex min-sm:flex-row min-sm:w-full'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#1c2b2d] max-sm:text-left max-sm:mt-3  max-sm:-ml-2  text-white font-mono text-xl `}>
-                        <li><Link to={`/`}>Home</Link></li>
-                        <li><Link to={`/games`}>Games</Link></li>
-                <li><Link to={`/articles`}>Articles</Link></li>
-                <li><Link to={`/users`}>Users</Link></li>
+            <ul className={`border-[#1c2b2d] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex ' : 'hidden min-sm:flex min-sm:flex-row min-sm:w-full'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#1c2b2d] max-sm:text-left max-sm:mt-3  max-sm:-ml-2  text-white font-mono text-xl `}>
+                        <li className="hover:text-[#d2eb47]"><Link to={`/`}>Home</Link></li>
+                        <li className="hover:text-[#d2eb47]"><Link to={`/games`}>Games</Link></li>
+                <li className="hover:text-[#d2eb47]"><Link to={`/articles`}>Articles</Link></li>
+                <li className="hover:text-[#d2eb47]"><Link to={`/users`}>Users</Link></li>
                     </ul>
                 </nav>
     )

fd8b83e81d4a9ab4fba37bf941584197510c421b
Asad Jawaid <asazycat@gmail.com>
2025-09-15 19:37:04 +0100

    homepage improved

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -10,17 +10,13 @@ export default function Homepage() {
 
     return (
         <>
-        <div className=" m-auto -bg-[#1c2b2d]   bg-[#1c2b2d] border-2 solid border-bg-[#1c2b2d]  w-9/10 mt-15 min-sm:flex min-sm:flex-row-reverse min-sm:rounded-2xl">
-            <img src={`${hero}`} className="m-auto size-150 rounded-2xl w-full"/>
-          
-                <div className="m-2 mt-10 min-sm:mt-50">
-            <p className="mt-5 text-white text-center font-extrabold bg-bg-[#1c2b2d]   bg-[#1c2b2d]  m-auto w-auto p-3 border-[#62afb9] border-5 rounded-2xl  ">From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</p>
-            
-                   
-        </div>
-        
-            </div>
-         
-        </>
+        <div className=" m-auto bg-[#1c2b2d]  min-sm:w-full min-sm:m-0">
+            <img src={`${hero}`} className="size-150 w-full opacity-35 min-sm:"/>
+                <div className="opacity-90 mt-5 text-white text-center font-extrabold bg-bg-[#1c2b2d]   bg-[#1c2b2d]  w-full p-3  absolute top-1/3 ">
+                    <p>From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</p> 
+                    <label className="hidden">Search</label><input type="text"  placeholder='search' className="w-1/3 mt-5 p-5 text-black bg-fuchsia-50 rounded-2xl"></input>
+                </div>
+       </div>
+         </>
             )
 }
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -11,17 +11,18 @@ export default function Profile() {
     if (user.name !== undefined) {
        return (
          <>
-            <div className="flex flex-row bg-[#1c2b2d]  justify-between" onClick={() => setMenu(true)}>
+            <div className="flex flex-row bg-[#1c2b2d]  justify-between" >
                  <div className="text-white font-extrabold mr-10 max-sm:hidden">
                    <h1>{user?.name}</h1>
-                       <p>Profile</p>
+                       <p onClick={() => setMenu(true)} className={`hover:text-[#d2eb47] ${menu ? 'text-[#d2eb47]' : ''}`}>Profile</p>
                        </div>  
-                <img src={`${user?.avatar_urls['48']}`} className="rounded-3xl"  onClick={() => setMenu(!menu)}/>
+                   <img src={`${user?.avatar_urls['48']}`} className=" max-sm:hidden rounded-3xl" />
+                   <img src={`${user?.avatar_urls['48']}`} className="min-sm:hidden rounded-3xl"  onClick={() => setMenu(!menu)}/>
                 
             </div>
-                <div className={`${menu ? `flex` : `hidden`} flex-col absolute max-sm:bg-[#1c2b2d]   bg-[#1c2b2d] mt-2 text-white`} onMouseEnter={() => setMenu(true)} onMouseLeave={() => setMenu(false)}>
-                    <button>View Profile</button>
-                    <button onClick={() => { localStorage.removeItem('tokenObj'); window.location.reload();}}>Logout</button>
+                <div className={`${menu ? `flex` : `hidden`} flex-col absolute max-sm:bg-[#1c2b2d]   bg-[#1c2b2d] mt-2 text-white p-2`} onMouseEnter={() => setMenu(true)} onMouseLeave={() => setMenu(false)}>
+                    <button className="hover:text-[#d2eb47] ">View Profile</button>
+                    <button onClick={() => { localStorage.removeItem('tokenObj'); window.location.reload();}} className="hover:text-[#d2eb47]">Logout</button>
                 </div>
                 </>
         )

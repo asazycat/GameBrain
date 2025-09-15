f171a3d478fbd04fac82e162785882a1766c0247
Asad Jawaid <asazycat@gmail.com>
2025-09-15 20:09:25 +0100

    homepage improved

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -14,7 +14,7 @@ export default function Homepage() {
             <img src={`${hero}`} className="size-150 w-full  min-sm:opacity-35"/>
                 <div className="opacity-90 mt-5 text-white text-center font-extrabold bg-bg-[#1c2b2d]   bg-[#1c2b2d]  w-full p-3  absolute top-1/3 ">
                     <p>From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</p> 
-                    <label className="hidden">Search</label><input type="text"  placeholder='search' className="w-1/3 mt-5 p-5 text-black bg-fuchsia-50 rounded-2xl"></input>
+                    <label className="hidden">Search</label><input type="text"  placeholder='not working at the moment' className="w-1/3 mt-5 p-5 text-black bg-fuchsia-50 rounded-2xl"></input>
                 </div>
        </div>
          </>

e2ab12494490a739ec431da630b76a8b01c7bb3a
Asad Jawaid <asazycat@gmail.com>
2025-09-15 19:46:11 +0100

    homepage improved

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -11,7 +11,7 @@ export default function Homepage() {
     return (
         <>
         <div className=" m-auto bg-[#1c2b2d]  min-sm:w-full min-sm:m-0">
-            <img src={`${hero}`} className="size-150 w-full opacity-35 min-sm:"/>
+            <img src={`${hero}`} className="size-150 w-full  min-sm:opacity-35"/>
                 <div className="opacity-90 mt-5 text-white text-center font-extrabold bg-bg-[#1c2b2d]   bg-[#1c2b2d]  w-full p-3  absolute top-1/3 ">
                     <p>From Action-packed to Visual Novel. RPG, Adventure, Strategy and more. Explore a variety of games from all the ages.</p> 
                     <label className="hidden">Search</label><input type="text"  placeholder='search' className="w-1/3 mt-5 p-5 text-black bg-fuchsia-50 rounded-2xl"></input>
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -7,7 +7,7 @@ export default function Navigation() {
     return (
         <nav className='w-1/2 max-sm:w-1/3'>
             <button className={`border-[#1c2b2d] border-2 border-solid text-[#3887a3] max-sm:block  bg-[#1c2b2d] max-sm:border-[#1C2B2D] w-full p-2 min-sm:hidden `} onClick={() => setBurger(!burger)}><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p><p className="border-2 mt-1 w-8 text-xl"></p></button>
-            <ul className={`border-[#1c2b2d] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex ' : 'hidden min-sm:flex min-sm:flex-row min-sm:w-full'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#1c2b2d] max-sm:text-left max-sm:mt-3  max-sm:-ml-2  text-[#3887a3] font-mono text-xl `}>
+            <ul className={`border-[#1c2b2d] border-2 p-2 justify-around ${ burger ?  ' min-sm:flex ' : 'hidden min-sm:flex min-sm:flex-row min-sm:w-full'} max-sm:absolute max-sm:w-full max-sm:h-full bg-[#1c2b2d] max-sm:text-left max-sm:mt-3  max-sm:-ml-2  text-white font-mono text-xl `}>
                         <li><Link to={`/`}>Home</Link></li>
                         <li><Link to={`/games`}>Games</Link></li>
                 <li><Link to={`/articles`}>Articles</Link></li>

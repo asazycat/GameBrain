cf69d01029e19a511ad5e29029ad9d746b6c81b9
Asad Jawaid <asazycat@gmail.com>
2025-09-15 20:18:38 +0100

    images

--- src/Components/EachSearchResult.tsx
+++ src/Components/EachSearchResult.tsx
@@ -9,14 +9,14 @@ export default function EachSearchResult({ game }: {game: IGame}) {
     return (
         <>
         <Link to={`${id}`}>
-            <li key={id} className=" flex flex-col-reverse max-w-full bg-[#1c2b2d] ">
+            <li key={id} className=" flex flex-col-reverse max-w-full bg-[#1c2b2d] h-full">
                 
                 <div className=" p-3 "> 
                      <p className=" p-2 text-sm  text-center w-auto max-w-fit border-2 bg-[#77858f] text-[#d2eb47] border-[#77858f] rounded-3xl m-0">{genre}</p>
                     <h1 className=" text-white font-mono text-2xl p-5 text-center">{name}</h1>   
                         <div className="p-2 flex  font-mono text-white w-full justify-around"> <Rating rating={rating} /> </div>
                  </div>
-                    <img src={image ?? ImageJPEG} alt={name} className=" h-[15em] w-full "/>
+                    <img src={image ?? ImageJPEG} alt={name} className=" h-full w-full "/>
                   
             </li>
             </Link>

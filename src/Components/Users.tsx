3d6f4b16c25501593457336ad131ffedb116ff88
Asad Jawaid <asazycat@gmail.com>
2025-09-16 06:28:17 +0100

    users received

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -1,3 +1,18 @@
+import { useContext, useState, useEffect } from "react"
+import localSiteDemo from "../../public/localSiteDemo"
+import { LoginContextProvider } from "../Contexts/LoginContextProvider"
+import type { IUser } from "../interface"
+
 export default function Users() {
-    return <>Users</>
+   const tokenObj = useContext(LoginContextProvider)
+    const [users, setUsers] = useState<IUser[]>([])
+    useEffect(() => {
+          (async function () {
+      return await fetch(`${localSiteDemo}/wp/v2/users`, { headers: { Authorization: `Bearer ${tokenObj.token}` } })
+          })().then((res) => res.json()).then((res) => { setUsers(res); console.log(res); })
+    },[tokenObj.token])
+console.log(users)
+    return (
+        <ul>{users.map((eachUser: IUser) => <li>{eachUser.name}</li>) }</ul>
+    )
 }

349547c1d17a9f216a34286ec6e82ea790737a7b
Asad Jawaid <asazycat@gmail.com>
2025-09-16 06:29:03 +0100

    users received

--- public/apiKey.ts
+++ public/apiKey.ts
@@ -1,9 +1,9 @@
 
 
 // replace string with your own
-// const apiKey = 'd13baa727ae441a29f0db05328ea730d' 
+const apiKey = 'd13baa727ae441a29f0db05328ea730d' //for portfolio
 
-const apiKey = '247d8a9a1b3440a9ae46ad65352f13b0'    
+// const apiKey = '247d8a9a1b3440a9ae46ad65352f13b0'    //for my development
 export default apiKey
 
 

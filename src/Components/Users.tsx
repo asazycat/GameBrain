6fd557af0ff128c60d5cb74398fdd8a344e70d00
Asad Jawaid <asazycat@gmail.com>
2025-09-28 17:29:37 +0100

    login page only comes when you want to -pt1

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -4,13 +4,13 @@ import { LoginContextProvider } from "../Contexts/LoginContextProvider"
 import type { IUser } from "../interface"
 
 export default function Users() {
-   const tokenObj = useContext(LoginContextProvider)
+   const login = useContext(LoginContextProvider)
     const [users, setUsers] = useState<IUser[]>([])
     useEffect(() => {
           (async function () {
-      return await fetch(`${localSiteDemo}/wp/v2/users`, { headers: { Authorization: `Bearer ${tokenObj.token}` } })
+      return await fetch(`${localSiteDemo}/wp/v2/users`, { headers: { Authorization: `Bearer ${login.tokenObj.token}` } })
           })().then((res) => res.json()).then((res) => { setUsers(res); })
-    },[tokenObj.token])
+    },[login.tokenObj.token])
 
     return (
         <ul>{users.map((eachUser: IUser) => <li>{eachUser.name}</li>) }</ul>

2f8b3cb941e8247e66c403e5f58f4220961e6b2a
unknown <asazycat@gmail.com>
2026-02-05 23:42:59 +0000

    change

--- src/Components/LoginPage.tsx
+++ src/Components/LoginPage.tsx
@@ -30,7 +30,7 @@ export default function LoginPage({setTokenObj}: {setTokenObj:Dispatch<SetStateA
         <form onSubmit={handleSubmit} className="flex flex-col w-3/4 m-auto p-5 bg-[#1c2b2d]  border-2 solid rounded-2xl h-[10em] justify-between mt-50 min-sm:w-1/2 min-md:w-1/3 min-lg:w-1/4 min-2xl:w-1/3 min-2xl:h-[20em]">
             <input className='bg-white text-gray-400 min-2xl:h-[3em]' type="text" onChange={e => setUsername(e.target.value)} placeholder='Username' value={username}/>
             <input className='bg-white text-gray-400 min-2xl:h-[3em]' type="password" onChange={e => setPassword(e.target.value)} placeholder='Password' value={password}/>
-            <p className="text-[#d2eb47]" onClick={() => { setUsername('DemoUserAuthor'); setPassword('Shifu6270abc')}}>Demo Account</p>
+            <p className="text-[#d2eb47]" onClick={() => { setUsername('DemoAccountUser'); setPassword('Shifu6270abc')}}>Demo Account</p>
             <div className="flex flex-row justify-around">
                 <button type='submit' className="bg-[#77858f] text-[#d2eb47] border-[#77858f]  border-2 border-solid p-2 rounded-2xl " >Login</button>
                <button className="bg-[#77858f] text-[#d2eb47] border-[#77858f]  border-2 border-solid p-2 rounded-2xl" onClick={() => setSignUp(false)}>Sign Up</button>

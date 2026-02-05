440f224d513f441412c4b4e80d85387d07ec7b3f
unknown <asazycat@gmail.com>
2026-02-05 23:39:07 +0000

    change

--- public/localSiteDemo.ts
+++ public/localSiteDemo.ts
@@ -1,6 +1,6 @@
 
 
-// const localSiteDemo = `https://gamebrain.gamer.gd/wp-json`
+// const localSiteDemo = ` https://gamebrain.online/wp-json`
 const localSiteDemo = `http://gamebrain.local/wp-json`
 export default localSiteDemo
 
